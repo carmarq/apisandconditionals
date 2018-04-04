4fcfd11924dc9566897d9b044f477109c92d4845
Carlos Marquez <carl.marq95@gmail.com>
2018-04-03 21:38:58 -0600

    Worked on understanding Objects such as name or color On interfaces and on APIS

---
+++ objects-apis.js
@@ -0,0 +1,26 @@
+/*
+*Objects, Interfaces, and APIS's
+ */
+
+/*let dog = {
+		name: "Steve",
+		color: "brown",
+		breed: "Pug",
+		size: "Small",
+		bark: function(typeOfBark){
+		console.log("Bark!");
+	},
+};
+*/
+
+function x(y) {
+	y = y.num + 5;
+	console.log(y);
+}
+
+let y = {
+	name:"Tom",
+	num: 10,
+};
+x(y);
+console.log(y);
